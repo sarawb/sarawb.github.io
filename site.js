f6263880c76f0d487f1e93877166dfbdf676c8bd
William Bolton <william.ellet@gmail.com>
2017-07-09 10:22:03 -0400

    Consulting and research section designed

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
-console.log("Hello!!")
+$(function() {
+
+  function changeConsultingArea() {
+    var width = $(window).width();
+    if(width >= 850) {
+      $(".consulting-areas-wrapper").removeClass("twelve").addClass("six");
+    } else {
+      $(".consulting-areas-wrapper").addClass("twelve").removeClass("six");
+    };
+  };
+
+  changeConsultingArea();
+  $( window ).resize(function() {
+    changeConsultingArea();;
+  });
+
+});
