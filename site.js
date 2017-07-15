a32e3858c886a7449e695a5b93cfdb35f99c21f4
William Bolton <william.ellet@gmail.com>
2017-07-15 10:16:30 -0400

    Resizing js for about

--- site.js
+++ site.js
@@ -18,11 +18,24 @@ $(function() {
     };
   }
 
+  function changeAbout() {
+    var width = $(window).width();
+    if(width >= 750) {
+      $("#swb-img-wrapper").addClass("four columns");
+      $("#about-content-wrapper").addClass("eight columns");
+    } else {
+      $("#swb-img-wrapper").removeClass("four columns");
+      $("#about-content-wrapper").removeClass("eight columns");
+    };
+  };
+
   changeConsultingArea();
   changeClients();
+  changeAbout();
   $( window ).resize(function() {
     changeConsultingArea();
     changeClients();
+    changeAbout();
   });
 
 });
